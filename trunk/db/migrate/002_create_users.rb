dc4a0ea10decffc0e6b45dddae423be02dc69e9c
caiomoritz <caiomoritz@06f0f101-6836-0410-985b-bb39fe77c459>
2007-08-05 20:16:31 +0000

    Migrated the User unit tests to Specs.

--- trunk/db/migrate/002_create_users.rb
+++ trunk/db/migrate/002_create_users.rb
@@ -1,13 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.column 'login',            :string,   :null => false, :limit => 40
-      t.column 'crypted_password', :string,   :null => false, :limit => 40
-      t.column 'first_name',       :string,   :null => false, :within => 2..60
-      t.column 'last_name',        :string,   :null => false, :within => 2..60
-      t.column 'email',            :string,   :null => false, :limit => 100
-      t.column 'created_at',       :datetime, :null => false
-      t.column 'updated_at',       :datetime, :null => false
+      t.column 'username', :string
+      t.column 'crypted_password', :string
+      t.column 'first_name', :string
+      t.column 'last_name', :string
+      t.column 'email', :string
+      t.column 'description', :string
+      t.column 'created_at', :datetime
+      t.column 'updated_at', :datetime
     end
   end
 
