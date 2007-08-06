9146efe24627457ad5be395a8479733baeaae915
caiomoritz <caiomoritz@06f0f101-6836-0410-985b-bb39fe77c459>
2007-08-06 12:52:46 +0000

    These are the first steps in the authentication system construction.

---
+++ trunk/test/functional/login_controller_test.rb
@@ -0,0 +1,45 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'login_controller'
+
+# Re-raise errors caught by the controller.
+class LoginController; def rescue_action(e) raise e end; end
+
+class LoginControllerTest < Test::Unit::TestCase
+  fixtures :users
+
+  def setup
+    @controller = LoginController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+  def test_index_without_user
+    get :index
+    assert_redirected_to :action => 'login'
+    assert_equal 'Please log in', flash[:notice]
+  end
+
+  def test_login_with_invalid_user
+    post :login, :user => {:username => 'bart', :password => 'bart2007'}
+    assert_response :success
+    assert_equal 'Invalid user/password combination', flash[:notice]
+  end
+
+  def test_login_with_valid_user
+    post :login, :user => {:username => 'bart', :password => 'test'}
+    assert_redirected_to :action => 'index'
+    assert_not_nil session[:user_id]
+    user = User.find(session[:user_id])
+    assert_equal 'bart', user.username
+  end
+
+  def test_logout
+    post :login, :user => {:username => 'bart', :password => 'test'}
+    assert_redirected_to :action => 'index'
+    assert_not_nil session[:user_id]
+    get :logout
+    assert session[:user_id].nil?
+    assert_redirected_to :action => 'login'
+    assert_equal 'You are now logged out', flash[:notice]
+  end
+end
