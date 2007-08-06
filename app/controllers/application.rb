9b810fb57f1ec788e311a72c9906a6201976772a
caiomoritz <caiomoritz@06f0f101-6836-0410-985b-bb39fe77c459>
2007-08-06 22:29:24 +0000

    Just to make the login screen look better.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,4 +4,11 @@
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_astra_session_id'
+
+  def authorize
+    unless session[:user_id]
+      flash[:notice] = 'Please log in'
+      redirect_to :controller => 'login', :action => 'login'
+    end
+  end
 end
