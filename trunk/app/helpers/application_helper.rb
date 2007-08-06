5375a317c1bf20034a0b00af33ebd4257599d1a7
caiomoritz <caiomoritz@06f0f101-6836-0410-985b-bb39fe77c459>
2007-08-06 14:01:11 +0000

    Now we have an empty main page.

--- trunk/app/helpers/application_helper.rb
+++ trunk/app/helpers/application_helper.rb
@@ -1,3 +1,18 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def login_info
+    if session[:user_id]
+      "You are logged in as #{session[:user_id].full_name}"
+    else
+      "You are not logged in"
+    end
+  end
+
+  def login_or_logout
+    if session[:user_id]
+      link_to 'Logout', :controller => 'login', :action => 'logout'
+    else
+      link_to 'Login', :controller => 'login', :action => 'login'
+    end
+  end
 end
